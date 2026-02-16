90f4320104ee91766022a484c75cc54e172fc351
pehhhziqi <ziqipeh@gmail.com>
2026-02-16 17:20:34 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const message = document.getElementById("message");
+
+const RSVP_LINK = "https://your-rsvp-link-here.com";
+
+let scale = 1;
+let clicks = 0;
+
+const texts = [
+  "No",
+  "Are you sure?",
+  "Really?",
+  "Think again 🥺",
+  "Pls 😭",
+  "You sure anot 💍"
+];
+
+noBtn.addEventListener("click", () => {
+  clicks++;
+  scale *= 0.8;
+  noBtn.style.transform = `scale(${scale})`;
+  noBtn.innerText = texts[clicks] || "…";
+});
+
+yesBtn.addEventListener("click", () => {
+  message.innerText = "YAY 💖 Redirecting you to the invite…";
+  setTimeout(() => {
+    window.location.href = RSVP_LINK;
+  }, 1500);
+});
